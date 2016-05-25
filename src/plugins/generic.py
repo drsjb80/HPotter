0855d41a3196aba9508a2f5f8b9e9477ccd57774
Steve Beaty <drsjb80@gmail.com>
2016-05-25 11:15:37 -0600

    initial

---
+++ src/plugins/generic.py
@@ -0,0 +1,32 @@
+import socket
+import socketserver
+
+# pass in socket, DB connection, logger
+
+def get_address(): return ('127.0.0.1', 2000)
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+        self.request.sendall(self.data.upper())
+
+# help from
+# http://cheesehead-techblog.blogspot.com/2013/12/python-socketserver-and-upstart-socket.html
+class MyServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    def __init__(self, mysocket):
+        self.mysocket = mysocket
+        socketserver.ThreadingMixIn.__init__(self)
+        socketserver.TCPServer.__init__(self, None, MyTCPHandler)
+
+    def server_bind(self):
+        print('in server_bind')
+        self.socket = self.mysocket
+
+
+if __name__ == "__main__":
+    mysocket = socket.socket()
+    mysocket.bind(get_address())
+    server = MyServer(mysocket)
+    server.serve_forever()
