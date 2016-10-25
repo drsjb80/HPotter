6b4544c02cce26cc37ba239e7fb47e8926da27fb
Steven Beaty <beatys@ms16142.local>
2016-10-25 14:24:52 -0600

    Initial checkin.

---
+++ src/hpotter/framework/HPotterDB.py
@@ -0,0 +1,22 @@
+from sqlalchemy.ext.declarative import declared_attr, declarative_base
+from sqlalchemy_utils import IPAddressType
+from sqlalchemy import Column, Integer, DateTime, func
+
+# https://www.ietf.org/rfc/rfc1700.txt
+TCP = 6
+UDP = 17
+
+class HPotterDB(object):
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+    id =  Column(Integer, primary_key=True)
+    created_at = Column(DateTime, default=func.now())
+    sourceIP = Column(IPAddressType)
+    sourcePort = Column(Integer)
+    destIP = Column(IPAddressType)
+    destPort = Column(Integer)
+    proto = Column(Integer)
+
+Base = declarative_base()
