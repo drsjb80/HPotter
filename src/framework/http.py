0604c0c26a018be71c3a7e82f1c9ecb4e9383e3e
Tyler <tsheets@msudenver.edu>
2017-10-05 13:01:24 -0600

    Changes to http.py and added empty etc files

--- src/framework/http.py
+++ src/framework/http.py
@@ -7,21 +7,19 @@ from datetime import *
 #def http_response():
 #ip = socket.gethostbyname(socket.AF_INET)
 
-
-print "--" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "--" , "http://www.ourserver.com/"
+print "--" + datetime.now().strftime('%Y %b %d %H:%M:%S') + "--" , "http://www.ourserver.com/"
 print "Resolving ourserver.com... " , (socket.gethostbyname(socket.gethostname()))
 print "Connecting to ourserver.|" + (socket.gethostbyname(socket.gethostname())) +"|" +  "... connected"
 print "HTTP request sent, awaiting response..."
 print "  HTTP/1.1 200 OK"
-print "  Date: ", datetime.now()
+print "  Date: ", datetime.now().strftime("%a, %d %b %y %H:%M:%S"), "GMT"
 print "  Server: Apache/2.4.6 (Red Hat Enterprise Linux) OpenSSL/1.0.2k-fips mod_fcgid/2.3.9 PHP/5.4.16"
-print "  Last-Modified: "
+print "  Last-Modified: ", datetime.today().strftime("%a, %d %b %y %H:%M:%S"), "GMT"
 print "  Accept-Ranges: bytes"
-print "  Content-Length:"
+print "  Content-Length:", "44911"
 print "  Cache-Control: max-age=0"
-print " ", datetime.today() , ",", "  Expires: " , datetime.now() + timedelta(hours=12)
+print "  Expires", datetime.today().strftime("%a, %d %b %y %H:%M:%S"), ",", "  Expires: ", datetime.today().strftime("%a, %d %b %y %H:%M:%S") + str(timedelta(hours=12))
 print "  Keep-Alive: timeout=5, max=100"
 print "  Connection: Keep-Alive"
 print "  Content-Type: text/html; charset=UTF-8"
-print "Length: "
-print "Saving to: 'index.html        100%[===============================================>] 43.94K  --.-KB/s   in 0s"
+print " Length: 44911 (44k) [text/html]"
