c291a2a3ed0c6ab42628f54b488e21033180c911
drsjb80 <drsjb80@gmail.com>
2018-09-06 09:09:07 -0600

    Moved the JSON server port number to env.py and fixed an encoding error.

--- hpotter/env.py
+++ hpotter/env.py
@@ -7,3 +7,5 @@ logger = logging.getLogger('hpotter')
 # note sqlite:///:memory: can't be used, even for testing, as it
 # doesn't work with threads.
 db = 'sqlite:///main.db'
+
+jsonserverport = 8000
