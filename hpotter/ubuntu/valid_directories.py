bf9df06c4f5ad87315559856068767942e624e73
Jonathan Nieblas <jniebla1@msudenver.edu>
2018-11-04 14:34:29 -0700

    Fixed some bugs for ubuntu_container.py. Also did some refactoring.

---
+++ hpotter/ubuntu/valid_directories.py
@@ -0,0 +1,3 @@
+# Keep valid directories from ubuntu here
+# Write block that executes some commands, then appends each directory to a list.
+# Save lists and user for comparisons, detecting wrong directories input by the user
