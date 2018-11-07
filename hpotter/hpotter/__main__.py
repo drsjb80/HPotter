09513dd13cc3a6b8a714edb3ca548ab29fd7704e
Jonathan Nieblas <jniebla1@msudenver.edu>
2018-11-07 01:20:18 -0700

    SSH server restarts after shutting down, create exception for FileNotFound error in ubuntu_container

--- hpotter/hpotter/__main__.py
+++ hpotter/hpotter/__main__.py
@@ -13,28 +13,9 @@ servers = []
 imported = ['__builtins__', 'types', 'socket', 'sqlalchemy', 'logging',
             'signal', 'env', 'HPotterDB', 'ubuntu_container']
 
-if "__main__" == __name__:
+
+def start_ssh_server(engine):
     global transport
-
-    engine = create_engine(db, echo=True)
-    Base.metadata.create_all(engine)
-
-    for name, val in list(globals().items()):
-        if isinstance(val, types.ModuleType):
-            if name in imported or name.__contains__('ssh'):
-                continue
-            for address in val.get_addresses():
-                mysocket = socket.socket(address[0])
-
-                try:
-                    mysocket.bind((address[1], address[2]))
-                    servers.append(val.start_server(mysocket, engine))
-                except OSError as e:
-                    print("bind to", address[1], address[2], e.strerror)
-
-    ubuntu_container.check_docker_version()
-
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     for name, val in list(globals().items()):
         if name.__contains__('ssh'):
@@ -48,6 +29,33 @@ if "__main__" == __name__:
                         mysocket.bind((address[1], address[2]))
                         server, transport = val.start_server(mysocket, engine)
                         servers.append(server)
-                        ssh.channel_handler(transport, server)
+                        ssh.channel_handler(transport, server, mysocket)
                     except OSError as e:
                         print("bind to", address[1], address[2], e.strerror)
+
+
+def start_servers():
+    global local_engine
+    local_engine = create_engine(db, echo=True)
+    Base.metadata.create_all(local_engine)
+
+    for name, val in list(globals().items()):
+        if isinstance(val, types.ModuleType):
+            if name in imported or name.__contains__('ssh'):
+                continue
+            for address in val.get_addresses():
+                mysocket = socket.socket(address[0])
+
+                try:
+                    mysocket.bind((address[1], address[2]))
+                    servers.append(val.start_server(mysocket, local_engine))
+                except OSError as e:
+                    print("bind to", address[1], address[2], e.strerror)
+
+
+if "__main__" == __name__:
+
+    start_servers()
+    ubuntu_container.check_docker_version()
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    start_ssh_server(local_engine)
