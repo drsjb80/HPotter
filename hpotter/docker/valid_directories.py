edec575532d1f6be7a32111384c9218ebd0bf921
Jonathan Nieblas <jniebla1@msudenver.edu>
2018-11-16 16:20:37 -0700

    Switch to Docker SDK, removing subprocess dependencies

--- hpotter/docker/valid_directories.py
+++ hpotter/docker/valid_directories.py
@@ -1,3 +1,3 @@
-# Keep valid directories from ubuntu here
+# Keep valid directories from docker here
 # Write block that executes some commands, then appends each directory to a list.
 # Save lists and user for comparisons, detecting wrong directories input by the user
