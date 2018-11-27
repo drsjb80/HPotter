58c4706debf870196026a192aa77cc5d5c5219df
Kyle <kylemanv@aol.com>
2018-11-27 15:32:13 -0700

    started ssh testing

---
+++ hpotter/test/test_shh.py
@@ -0,0 +1,10 @@
+import unittest
+from unittest.mock import Mock, call
+from hpotter.plugins.ssh import get_addresses
+
+class TestSsh(unittest.TestCase):
+	def setUp(self):
+		TelnetHandler.undertest=True
+ 
+	def test_get_addresses(self):
+		self.assertEqual(get_addresses(),[(socket.AF_INET, '0.0.0.0', 22)])
