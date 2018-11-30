94e3aff0e850b0586c1424e5ab857e406a21c7b4
Kyle <kylemanv@aol.com>
2018-11-29 22:05:28 -0700

    minor commit

--- hpotter/test/test_ssh.py
+++ hpotter/test/test_ssh.py
@@ -1,10 +1,11 @@
 import unittest
 from unittest.mock import Mock, call
-from plugins.ssh import get_addresses
+from hpotter.plugins.ssh import get_addresses
+from hpotter.plugins.ssh import SSHServer
 
 class TestSsh(unittest.TestCase):
 	def setUp(self):
-		TelnetHandler.undertest=True
+		SSHServer.undertest=True
  
 	def test_get_addresses(self):
 		self.assertEqual(get_addresses(),[(socket.AF_INET, '0.0.0.0', 22)])
---
+++ run-tests.cmd
@@ -0,0 +1,2 @@
+::run tests
+start /min cmd /c python -m unittest discover hpotter\test
