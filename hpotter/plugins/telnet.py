d9014264e8353e9d199e95fd0532acd4cee0f783
drsjb80 <drsjb80@gmail.com>
2018-12-05 14:28:56 -0700

    Moved telnet to use Linux docker, put in shutdown for each server, cleaned up the finding and using of plugins.

--- hpotter/plugins/telnet.py
+++ hpotter/plugins/telnet.py
@@ -5,15 +5,18 @@ from hpotter.hpotter import HPotterDB
 from hpotter.env import logger
 from hpotter.hpotter.command_response import command_response
 from hpotter.hpotter import consolidated
+
+import logging
+import docker
 import socket
 import socketserver
 import threading
-import unittest
+import re
+
 from unittest.mock import Mock, call
-from hpotter.docker import linux_container
 
-
-# Remember to put name in __init__.py
+_telnet_container = None
+_telnet_server = None
 
 # https://docs.python.org/3/library/socketserver.html
 class TelnetHandler(socketserver.BaseRequestHandler):
@@ -60,8 +63,8 @@ class TelnetHandler(socketserver.BaseRequestHandler):
         return response
 
     def fake_shell(self, socket, session, entry, prompt):
-        command, work_dir, cd = "", "base", "cd"
         command_count = 0
+        workdir = ''
         while command_count < 4:
             socket.sendall(prompt)
             command = self.get_string(socket)
@@ -69,18 +72,40 @@ class TelnetHandler(socketserver.BaseRequestHandler):
 
             if command == '':
                 continue
-            elif command.startswith(cd):
-                work_dir, dne = linux_container.change_directories(command)
-                if dne is True:
-                    dne_output = "\r\nbash: {}: command not found".format(command)
-                    socket.sendall(dne_output.encode("utf-8"))
-            elif command == 'exit':
+
+            if command.startswith('cd'):
+                directory = command.split(' ')
+                if len(directory) == 1:
+                    continue
+
+                directory = directory[1]
+
+                if directory == '.':
+                    continue
+
+                if directory == '..':
+                    workdir = re.sub(r'/[^/]*/?$', '', workdir)
+                    continue
+
+                if directory[0] != '/':
+                    workdir += '/'
+                workdir += directory
+
+                continue
+
+            if command == 'exit':
                 break
-            elif command in command_response:
-                socket.sendall(command_response[command].encode("utf-8"))
+
+            global _telnet_container
+            print(workdir)
+            exit_code, output = _telnet_container.exec_run(command,
+                workdir=workdir)
+
+            if exit_code == 126:
+                socket.sendall(command.encode('utf-8') + 
+                    b': command not found\n')
             else:
-                output = "\r\n" + linux_container.get_response(command, work_dir)
-                socket.sendall(output.encode("utf-8"))
+                socket.sendall(output)
 
             cmd = consolidated.CommandTable(command=command)
             cmd.hpotterdb = entry
@@ -98,9 +123,9 @@ class TelnetHandler(socketserver.BaseRequestHandler):
         if username == '':
             return
 
-        prompt = b'\r\n#: '
+        prompt = b'\n#: '
         if username == 'root' or username == 'admin':
-            prompt = b'\r\n$: '
+            prompt = b'\n$: '
 
         password = self.trying(b'Password: ', self.request)
         if password == '':
@@ -110,9 +135,10 @@ class TelnetHandler(socketserver.BaseRequestHandler):
         login.hpotterdb = entry
         self.session.add(login)
 
-        self.request.sendall(b'Last login: Mon Nov 20 12:41:05 2017 from 8.8.8.8\r\n')
+        self.request.sendall(b'Last login: Mon Nov 20 12:41:05 2017 from 8.8.8.8\n')
         
         self.fake_shell(self.request, self.session, entry, prompt)
+        self.request.close()
 
     def finish(self):
         # ugly ugly ugly
@@ -142,14 +168,29 @@ class TelnetServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     def server_bind(self):
         self.socket = self.mysocket
 
-
 # listen to both IPv4 and v6
 # quad 0 allows for docker port exposure
 def get_addresses():
     return [(socket.AF_INET, '0.0.0.0', 23)]
 
-
 def start_server(my_socket, engine):
-    server = TelnetServer(my_socket, engine)
-    server_thread = threading.Thread(target=server.serve_forever)
+    client = docker.from_env()
+
+    global _telnet_container
+    _telnet_container = client.containers.run('alpine', command=['/bin/ash'],
+        tty=True, detach=True, read_only=True)
+
+    network = client.networks.get('bridge')
+    network.disconnect(_telnet_container)
+
+    global _telnet_server
+    _telnet_server = TelnetServer(my_socket, engine)
+    server_thread = threading.Thread(target=_telnet_server.serve_forever)
     server_thread.start()
+
+def stop_server():
+    logging.info('Shutting down telnet server')
+    _telnet_server.shutdown()
+    _telnet_container.stop()
+    _telnet_container.remove()
+    logging.info('Done shutting down telnet server')
