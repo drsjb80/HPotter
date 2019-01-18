ffb1a2cef021bc5ddcc17fbc2a49e303ae3dc33a
drsjb80 <drsjb80@gmail.com>
2019-01-18 08:39:29 -0700

    created a general docker shell and some cleanup

--- hpotter/env.py
+++ hpotter/env.py
@@ -1,6 +1,7 @@
 import logging
 import logging.config
 import platform
+import docker
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from hpotter.hpotter.HPotterDB import Base
@@ -17,5 +18,18 @@ Session = scoped_session(sessionmaker(engine))
 
 # a start, for a Pi 0.
 machine = 'arm32v6/' if platform.machine() == 'armv6l' else ''
+
+busybox=True
+client = docker.from_env()
+if busybox:
+    shell_container = client.containers.run(machine + 'busybox', 
+        command=['/bin/ash'], tty=True, detach=True, read_only=True)
+else:
+    shell_container = client.containers.run(machine + 'alpine',
+        command=['/bin/ash'], user='guest', tty=True, detach=True,
+        read_only=True)
+
+network = client.networks.get('bridge')
+network.disconnect(shell_container)
 
 jsonserverport = 8000
