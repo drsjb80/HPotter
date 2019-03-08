5041b6a219e5fd145c2c1997af54d3679ab60409
drsjb80 <drsjb80@gmail.com>
2019-03-07 20:20:21 -0700

    Ready to share

--- hpotter/docker/shell.py
+++ hpotter/docker/shell.py
@@ -12,11 +12,8 @@ def get_string(client_socket, limit=4096, telnet=False):
     while telnet and character == b'\xff':
         # skip the next two as they are part of the telnet command
         client_socket.recv(1)
-        logger.debug(ord(character))
         client_socket.recv(1)
-        logger.debug(ord(character))
         character = client_socket.recv(1)
-        logger.debug(ord(character))
 
     string = ''
     while character not in (b'\n', b'\r'):
@@ -25,16 +22,18 @@ def get_string(client_socket, limit=4096, telnet=False):
         elif character == '\x15':   # control-u
             string = ''
         elif ord(character) > 127:
+            logger.debug('Meta character')
             raise UnicodeError('Meta character')
         elif len(string) > limit:
+            logger.debug('Too many characters')
             raise IOError('Too many characters')
         else:
             string += character.decode('utf-8')
 
         character = client_socket.recv(1)
-        logger.debug(ord(character))
         if not telnet:
             client_socket.send(character)
+
 
     if not telnet:
         client_socket.send(b'\n')
@@ -43,6 +42,7 @@ def get_string(client_socket, limit=4096, telnet=False):
     if telnet and character == b'\r':
         character = client_socket.recv(1)
 
+    logger.debug('get_string returing ' + string.strip())
     return string.strip()
 
 def fake_shell(client_socket, session, connection, prompt, telnet=False):
@@ -57,6 +57,7 @@ def fake_shell(client_socket, session, connection, prompt, telnet=False):
             command = get_string(client_socket, telnet=telnet)
             command_count += 1
         except:
+            logger.debug('get_string threw an exception')
             client_socket.close()
             break
 
@@ -83,17 +84,23 @@ def fake_shell(client_socket, session, connection, prompt, telnet=False):
 
             continue
 
+        logger.debug('Shell workdir ' + workdir)
+
         if command == 'exit':
             break
 
         cmd = tables.ShellCommands(command=command, connection=connection)
         session.add(cmd)
         session.commit()
+        logger.debug('Shell committed command')
 
         timeout = 'timeout 1 ' if get_busybox() else 'timeout -t 1 '
 
         exit_code, output = get_shell_container().exec_run(timeout + command, \
             workdir=workdir)
+
+        logger.debug('Shell exit_code ' + str(exit_code))
+        logger.debug('Shell output ' + str(output))
 
         output = output.replace(b'\n', b'\r\n')
 
