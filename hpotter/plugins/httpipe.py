132d20a4616ce1f9aa07509e7d5f8275180682ef
drsjb80 <drsjb80@gmail.com>
2019-03-18 13:56:24 -0600

    Before creating generic pipe file

--- hpotter/plugins/httpipe.py
+++ hpotter/plugins/httpipe.py
@@ -1,7 +1,5 @@
 import socket
 import threading
-import platform
-import tempfile
 
 import docker
 
@@ -13,25 +11,27 @@ from hpotter.env import logger, Session
 
 # started from: http://code.activestate.com/recipes/114642/
 
-class PipeThread(threading.Thread):
+class OneWayThread(threading.Thread):
     # pylint: disable=R0913
-    def __init__(self, source, dest, capture, limit=0):
-        threading.Thread.__init__(self)
+    def __init__(self, source, dest, table=None, limit=0):
+        super().__init__()
         self.source = source
         self.dest = dest
-        self.capture = capture
         self.limit = limit
-        self.session = Session()
+        self.table = table
 
-        self.connection = tables.Connections(
-            sourceIP=self.source.getsockname()[0],
-            sourcePort=self.source.getsockname()[1],
-            destIP=self.dest.getsockname()[0],
-            destPort=self.dest.getsockname()[1],
-            proto=tables.TCP)
-        self.session.add(self.connection)
+        if self.table:
+            self.session = Session()
+            self.connection = tables.Connections(
+                sourceIP=self.source.getsockname()[0],
+                sourcePort=self.source.getsockname()[1],
+                destIP=self.dest.getsockname()[0],
+                destPort=self.dest.getsockname()[1],
+                proto=tables.TCP)
+            self.session.add(self.connection)
 
     def run(self):
+        logger.debug('Starting timer')
         timer = threading.Timer(120, self.shutdown)
         timer.start()
 
@@ -39,55 +39,59 @@ class PipeThread(threading.Thread):
         while 1:
             try:
                 data = self.source.recv(4096)
-            except OSError as ose:
-                # closed connection
-                if ose.errno != 9:
-                    logger.info('recv')
+            except IOError as err:
+                # 9 = time out
+                if err.errno != 9:
+                    logger.info(err)
+                break;
+            except Exception as exc:
+                logger.info(exc)
                 break
 
-            # logger.info(data)
             if data == b'' or not data:
                 break
 
-            if self.capture:
+            if self.table:
                 total += data
 
             try:
                 self.dest.sendall(data)
-            except BaseException as exc:
-                logger.info('sendall')
+            except OSError as exc:
                 logger.info(exc)
                 break
 
             if self.limit > 0 and len(total) > self.limit:
                 break
 
-        if self.capture:
-            http = tables.HTTPCommands(request=str(total), \
-                connection=self.connection)
+        if self.table:
+            http = self.table(request=str(total), connection=self.connection)
             self.session.add(http)
             self.session.commit()
-            Session.remove()
 
-        logger.info('Canceling timer')
+        logger.debug('Canceling timer')
         timer.cancel()
         self.shutdown()
 
     def shutdown(self):
-        logger.info('PipeThread.shutdown')
+        if self.table:
+            Session.remove()
         self.source.close()
         self.dest.close()
 
-class HttpdThread(threading.Thread):
-    def __init__(self):
+class PipeThread(threading.Thread):
+    def __init__(self, bind_address, connect_address, table, limit):
         super().__init__()
+        self.bind_address = bind_address
+        self.connect_address = connect_address
+        self.table = table
+        self.limit = limit
         self.shutdown_requested = False
 
     def run(self):
         source_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         source_socket.settimeout(5)
-        source_socket.bind(('0.0.0.0', 80))
-        source_socket.listen(4)
+        source_socket.bind(self.bind_address)
+        source_socket.listen()
 
         while True:
             try:
@@ -100,58 +104,52 @@ class HttpdThread(threading.Thread):
                         continue
 
                 dest = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                logger.info(hpotter.env.httpd_container_address)
-                dest.connect(('127.0.0.1', 8080))
-                logger.info('after connect')
+                dest.settimeout(5)
+                dest.connect(self.connect_address)
 
-                PipeThread(source, dest, True, 4096).start()
-                PipeThread(dest, source, False).start()
+                OneWayThread(source, dest, self.table, self.limit).start()
+                OneWayThread(dest, source).start()
 
-                # to avoid a DOS attack, put two joins in here and/or keep
-                # track of the number of threads here. could create a list
-                # and join the first when "full"
-            except BaseException as exc:
+            except OSError as exc:
                 source.close()
-                logger.info('HttpdThread')
                 logger.info(exc)
                 continue
 
     def request_shutdown(self):
         self.shutdown_requested = True
 
-def start_server():
-    # machine = 'arm32v6/' if platform.machine() == 'armv6l' else ''
-    try:
-        client = docker.from_env()
-
-        hpotter.env.httpd_container = client.containers.run \
-        ( \
-            'httpd', \
-            detach=True, \
-            ports={'80/tcp': 8080}, \
-            read_only=True, \
-            volumes={'apache2': {'bind': '/usr/local/apache2', 'mode': 'rw'}} \
-        )
-        logger.info('Created: %s', hpotter.env.httpd_container)
-
-    except BaseException as exc:
-        logger.info(exc)
-        if hpotter.env.httpd_container:
-            logger.info(hpotter.env.httpd_container.logs())
-        return
-
-    hpotter.env.httpdThread = HttpdThread()
-    hpotter.env.httpdThread.start()
-
-def stop_server():
-    # hpotter.env.httpd_network.disconnect(hpotter.env.httpd_container)
-    # hpotter.env.httpd_network.remove()
-
-    hpotter.env.httpdThread.request_shutdown()
-
+def rm_container():
     if hpotter.env.httpd_container:
         logger.info('Stopping httpd_container')
         hpotter.env.httpd_container.stop()
         logger.info('Removing httpd_container')
         hpotter.env.httpd_container.remove()
         hpotter.env.httpd_container = None
+    else:
+        logger.info('No httpd_container to stop')
+
+def start_server():     # leave these two in place
+    # machine = 'arm32v6/' if platform.machine() == 'armv6l' else ''
+    try:
+        client = docker.from_env()
+
+        # create apache2 or random dir?
+        hpotter.env.httpd_container = client.containers.run('httpd:latest', \
+            detach=True, ports={'80/tcp': 8080}, read_only=True, \
+            volumes={'apache2': {'bind': '/usr/local/apache2', 'mode': 'rw'}})
+        logger.info('Created: %s', hpotter.env.httpd_container)
+
+    except OSError as err:
+        logger.info(err)
+        if hpotter.env.httpd_container:
+            logger.info(hpotter.env.httpd_container.logs())
+            rm_container()
+        return
+
+    hpotter.env.httpdThread = PipeThread(('0.0.0.0', 80), \
+        ('127.0.0.1', 8080), tables.HTTPCommands, 4096)
+    hpotter.env.httpdThread.start()
+
+def stop_server():
+    hpotter.env.httpdThread.request_shutdown()
+    rm_container()
