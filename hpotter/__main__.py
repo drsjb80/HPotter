aac3071f0af32fc3f5954acdffbac86e737348ef
drsjb80 <drsjb80@gmail.com>
2019-04-05 09:51:46 -0600

    Fixes for SQLite3 needing a mutex

--- hpotter/__main__.py
+++ hpotter/__main__.py
@@ -2,15 +2,11 @@ import importlib
 import signal
 
 import hpotter.plugins
-from hpotter.env import logger, stop_shell, Session
+from hpotter.env import logger, stop_shell, close_db
 
 plugins_dict = hpotter.plugins.__dict__
 
-session = Session()
-
 def shutdown_servers(signum, frame):
-    session.commit()
-    session.close()
     for plugin_name in plugins_dict['__all__']:
         importlib.import_module('hpotter.plugins.' + plugin_name)
         plugin = plugins_dict[plugin_name]
@@ -20,13 +16,14 @@ def shutdown_servers(signum, frame):
 
     # shell might have been started by telnet, ssh, ...
     stop_shell()
+    close_db()
 
 def startup_servers():
     for plugin_name in plugins_dict['__all__']:
         importlib.import_module('hpotter.plugins.' + plugin_name)
         plugin = plugins_dict[plugin_name]
         logger.info('Starting %s', plugin_name)
-        plugin.start_server(session)
+        plugin.start_server()
 
 if "__main__" == __name__:
     signal.signal(signal.SIGINT, shutdown_servers)
