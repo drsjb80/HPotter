592bb84a78969ad1f5a257c271369797c414371f
Robert <rob@Robs.Laptop>
2019-09-18 13:27:13 -0600

    added new componant drillDownWindow. This componant acts as a beginning of a system to dynamically shift content based upon selected KPI

--- hpotter/dashboard/src/App.vue
+++ hpotter/dashboard/src/App.vue
@@ -8,13 +8,29 @@
 
 <template>
   <v-app>
-    <v-navigation-drawer app>
-    </v-navigation-drawer>
+    <v-navigation-drawer app class="elevation-3"> <!--Sidebar-->
+    </v-navigation-drawer> <!--End Sidebar-->
     <v-content>
-      <v-container>
+      <v-container ma-2>
         <v-row>
-          <v-col><cards :kpi="kpi"/></v-col>
-          <v-col cols="4" class="d-none d-lg-block"><v-date-picker class="elevation-3"></v-date-picker></v-col>
+          <v-col> <!--Main Content-->
+
+            <v-row>
+              <cards :kpi="kpi"/>
+            </v-row>
+
+            <v-row>
+              <drillDownWindow />
+            </v-row>
+
+        </v-col> <!--End Main Content-->
+
+          <v-col cols="4" class="d-none d-lg-block"> <!--Right Bar-->
+
+            <v-date-picker class="elevation-3"></v-date-picker>
+
+          </v-col> <!--End Right Bar-->
+
           <div class="d-lg-none"><v-btn fixed dark fab bottom right color="primary"><v-icon>mdi-calendar</v-icon></v-btn></div>
         </v-row>
       </v-container>
@@ -25,6 +41,7 @@
 <script>
 
 import cards from './components/cards';
+import drillDownWindow from './components/drilldownwindow';
 
 export default {
 
@@ -33,6 +50,7 @@ export default {
   name: 'App',
   components: {
     cards,
+    drillDownWindow
   },
   data: () => ({
       kpi: [
---
+++ hpotter/dashboard/src/components/drilldownwindow.vue
@@ -0,0 +1,33 @@
+<style scoped></style>
+<template>
+  <v-window v-model="content">
+    <v-window-item :value="1">
+      <v-card ma-2 class="elevation-3">
+        <div> <!--Dummy Content -->
+        Attacks<br>
+        S	A	T	O	R<br>
+        A	R	E	P	O<br>
+        T	E	N	E	T<br>
+        O	P	E	R	A<br>
+        R	O	T	A	S
+        </div>
+      </v-card>
+    </v-window-item>
+
+    <v-window-item :value="2">
+      <v-card ma-2 class="elevation-3">
+        <div> <!--Dummy Content -->
+        Plug-ins<br>
+        S	A	T	O	R<br>
+        A	R	E	P	O<br>
+        T	E	N	E	T<br>
+        O	P	E	R	A<br>
+        R	O	T	A	S
+        </div>
+      </v-card>
+    </v-window-item>
+  </v-window>
+</template>
+
+
+<script></script>
