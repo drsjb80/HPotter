46c4dc4cb3653acc97b7d85425293660081ca842
Boykinmr <MBoykin@MSUDenver.edu>
2019-09-18 23:41:19 -0600

    HPOT-65: Finally got the unit tests working.  Give it a once-over and let me know if I am messing anything up.  Pull Request incoming.

--- hpotter/test/test_a_tables.py
+++ hpotter/test/test_a_tables.py
@@ -10,9 +10,12 @@ class TestTables(unittest.TestCase):
         result = checkForTables()
         if result == '1':
             #after we receive '1' the tables should be made and now return '5'
+            checkAgain = checkForTables()
+            if checkAgain == '5':print('Database built')
             self.assertEqual(checkForTables(), '5')
         else:
             self.assertEqual(result, '5')
+            print('Database exists')
 
 if __name__ == '__main__':
     unittest.main()
