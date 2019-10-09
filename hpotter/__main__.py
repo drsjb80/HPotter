48a95757322655359402a0baa2ee1a9465240ff5
Rflageolle <rflageol@msudenver.edu>
2019-10-09 11:27:22 -0600

    Update __main__.py

--- hpotter/__main__.py
+++ hpotter/__main__.py
@@ -4,22 +4,11 @@ import hpotter.plugins
 from hpotter.plugins.plugin_handler import start_plugins, stop_plugins
 from hpotter.env import logger, stop_shell, close_db
 
-plugins_dict = hpotter.plugins.__dict__
-
 def shutdown_servers(signum, frame):
     stop_plugins()
     # shell might have been started by telnet, ssh, ...
     stop_shell()
     close_db()
-
-# new startup for generic plugins launcher
-def startup_servers_new():
-    index = 0
-    for plugin_name in plugins_dict['__plugins__']:
-
-        logger.info('Starting %s', plugin_name)
-        start_server(plugin_name, index)
-        index = index + 1
 
 if "__main__" == __name__:
     signal.signal(signal.SIGTERM, shutdown_servers)
