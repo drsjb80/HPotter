9d486aac4898da6302c10a801a0c6d0caa4ad3f4
kclark85 <kclark85@msudenver.edu>
2019-10-12 17:14:15 -0600

    fixing signal handling for windows systems

--- hpotter/__main__.py
+++ hpotter/__main__.py
@@ -1,4 +1,4 @@
-import signal
+import signal, sys, win32api, inspect, os
 
 import hpotter.plugins
 from hpotter.plugins.plugin_handler import start_plugins, stop_plugins
@@ -10,9 +10,19 @@ def shutdown_servers(signum, frame):
     stop_shell()
     close_db()
 
+def shutdown_win_servers(signum):
+    stop_plugins()
+    # shell might have been started by telnet, ssh, ...
+    stop_shell()
+    close_db()
+
 if "__main__" == __name__:
-    signal.signal(signal.SIGTERM, shutdown_servers)
-    signal.signal(signal.SIGINT, shutdown_servers)
+    if sys.platform != 'win32':
+      signal.signal(signal.SIGTERM, shutdown_servers)
+      signal.signal(signal.SIGINT, shutdown_servers)
+    else:
+      win32api.SetConsoleCtrlHandler(shutdown_win_servers)
+      #win32api.SetConsoleCtrlHandler(signal.signal(signal.SIGINT,shutdown_servers))
 
     # startup_servers()
     start_plugins()
