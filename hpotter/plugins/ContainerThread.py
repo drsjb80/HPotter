c831d49d2940129eada592cb80574304f1cf0ddd
drsjb80 <drsjb80@gmail.com>
2020-01-15 15:09:28 -0700

    Pushing to Linux.

--- hpotter/plugins/ContainerThread.py
+++ hpotter/plugins/ContainerThread.py
@@ -10,24 +10,48 @@ class ContainerThread(threading.Thread):
         super().__init__()
         self.source = source
         self.dest = self.thread1 = self.thread2 = self.container = None
+        self.network = self.IPAddress = None
 
     def run(self):
         try:
             client = docker.from_env()
             self.container = client.containers.run( \
                 'httpd:latest', \
-                detach=True, \
-                ports={'80/tcp': 8080})
+                detach=True)
             logger.info('Created: %s', self.container)
         except Exception as err:
             logger.info(err)
             return
 
+        '''
         try:
-            logger.info("Connecting to ('127.0.0.1', 8080)");
+            self.network = client.networks.create('httpipe', driver='bridge')
+            logger.info('Created: %s', self.network)
+        except Exception as err:
+            logger.info(err)
+            self.stopAndRemove()
+            return
+        '''
+
+        '''
+        try:
+            self.network.connect(self.container)
+            self.network.reload()
+            logger.info(self.network.containers)
+        except Exception as err:
+            logger.info(err)
+            self.stopAndRemove()
+            return
+        '''
+
+        try:
+            self.container.reload()
+            logger.info(self.container.attrs['NetworkSettings']['Networks'])
+            self.IPAddress = self.container.attrs['NetworkSettings']['Networks']['bridge']['IPAddress']
+            logger.info('Connecting to (' + self.IPAddress + ', 8080)');
             self.dest = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.dest.settimeout(30)
-            self.dest.connect(('127.0.0.1', 8080))
+            self.dest.connect((self.IPAddress, 8080))
             logger.debug(str(self.dest))
         except Exception as err:
             logger.info(err)
