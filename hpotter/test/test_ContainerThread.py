31867f773a27e231e389950aa2203a386142f668
KenjiMorizono <kenji.morizono1@gmail.com>
2020-02-12 10:59:51 -0700

    iptc automatically adds a net mask to the IP if the sub net mask is not provided when specifying the ip in the rule. Added subnet mask to reflect this and added comment to clarify

--- hpotter/test/test_ContainerThread.py
+++ hpotter/test/test_ContainerThread.py
@@ -6,11 +6,13 @@ class TestContainerThread(unittest.TestCase):
 
     def test_TestDynamicFireWallRule1Src(self):
         source_ip = "192.168.1.1"
+        netmask = "255.255.255.255"     # When the net mask is not specified by the user, it defaults to this
         source_port = "8080"
         destination_ip = "172.161.1.1"
         destination_port = "8081"
 
         result = ContainerThread.create_rules(self, source_ip, source_port, destination_ip, destination_port)
+        expected_src = source_ip + "/" + netmask
 
-        self.assertEqual(result[0].src, source_ip)
+        self.assertEqual(result[0].src, expected_src)
         self.assertEqual(result[0].matches[0].sport, source_port)
