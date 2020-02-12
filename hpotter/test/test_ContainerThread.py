0b0f077c2671e3339e0943db2d998f9cbe8e5f77
KenjiMorizono <kenji.morizono1@gmail.com>
2020-02-12 11:59:26 -0700

    Added another test Added another test stub which needs some thinking to ensure the proper flag is set when creating the rule

--- hpotter/test/test_ContainerThread.py
+++ hpotter/test/test_ContainerThread.py
@@ -4,7 +4,7 @@ from hpotter.plugins.ContainerThread import ContainerThread
 
 class TestContainerThread(unittest.TestCase):
 
-    def test_TestDynamicFireWallRule1(self):
+    def test_TestDynamicFirewallRule1(self):
         source_ip = "192.168.1.1"
         netmask = "255.255.255.255"     # When the subnet mask is not specified by the user, it defaults to this
         source_port = "8080"
@@ -12,10 +12,38 @@ class TestContainerThread(unittest.TestCase):
         destination_port = "8081"
 
         result = ContainerThread.create_rules(self, source_ip, source_port, destination_ip, destination_port)[0]
-        expected_src = source_ip + "/" + netmask
-        expected_dst = destination_ip + "/" + netmask
+        real_src = source_ip + "/" + netmask
+        real_dst = destination_ip + "/" + netmask
 
-        self.assertEqual(result.src, expected_src)
+        self.assertEqual(result.src, real_src)
         self.assertEqual(result.matches[0].sport, source_port)
-        self.assertEqual(result.dst, expected_dst)
+        self.assertEqual(result.dst, real_dst)
         self.assertEqual(result.matches[0].dport, destination_port)
+        self.assertEqual(result.protocol, "tcp")
+
+    def test_TestDynamicFirewallRule2(self):
+        source_ip = "192.168.1.1"
+        netmask = "255.255.255.255"  # When the subnet mask is not specified by the user, it defaults to this
+        source_port = "8080"
+        destination_ip = "172.161.1.1"
+        destination_port = "8081"
+
+        result = ContainerThread.create_rules(self, source_ip, source_port, destination_ip, destination_port)[1]
+        real_src = source_ip + "/" + netmask
+        real_dst = destination_ip + "/" + netmask
+
+        self.assertEqual(result.src, real_dst)
+        self.assertEqual(result.matches[0].sport, destination_port)
+        self.assertEqual(result.dst, real_src)
+        self.assertEqual(result.matches[0].dport, source_port)
+        self.assertEqual(result.protocol, "tcp")
+
+    # TODO: Since this rule inverts the destination there needs to be a way to make sure that the rule is inverted
+    # The actual destination address is not changed. Relevant lines in iptc.py [1111 - 1153]
+    '''
+    def test_TestDynamicFirewallRule3(self):
+        print("TODO")
+    '''
+
+
+
