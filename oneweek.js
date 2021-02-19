f4dbe304652843e6fca0e2d35660aa9e16ff3b13
rsiverd-msu <rsiverd@msudenver.edu>
2021-02-18 18:55:06 -0700

    Adding in clustering for points

--- oneweek.js
+++ oneweek.js
@@ -1,14 +1,22 @@
 const url = "http://localhost:8080/"
 let myMap;
+let iw;
 
 let script = document.createElement('script');
 script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyA0P5XjTSHj7t2nmNL7_x8d5TGaxQYse4s&callback=initMap"
 script.async = true;
 
 function initMap() {
-   myMap = new google.maps.Map(document.getElementById('map'),
-     { center: {lat: 0, lng: 0}, zoom: 2});
-   fetchAllLocations();
+  myMap = new google.maps.Map(document.getElementById('map'),
+    { center: {lat: 0, lng: 0}, zoom: 2});
+  iw = new google.maps.InfoWindow();
+
+  oms = new OverlappingMarkerSpiderfier(myMap, {
+    markersWontMove: true,
+    markersWontHide: true,
+    basicFormatEvents: true
+  });
+  fetchAllLocations();
 }
 
 document.head.appendChild(script);
@@ -30,29 +38,40 @@ function createPointMarker(node) {
   let geom = new google.maps.LatLng(node.latitude, node.longitude);
   let marker = new google.maps.Marker({
     position: geom,
-    map: myMap,
   });
    
   let information = new google.maps.InfoWindow({
     content: getContentHTML(node),
   });
-   
-  marker.addListener('click', () => {
-    information.open(myMap, marker);
-  });
+ 
+  google.maps.event.addListener(marker, 'spider_click', function(e) {
+    iw.setContent(getContentHTML(node));
+    iw.open(myMap, marker);
+  }); 
+  oms.addMarker(marker); 
+
+  return marker;
 }
 
 function createMarkers(edges) {
+  let markers = [];
   if(Array.isArray(edges)) {
-    edges.forEach(edge => createPointMarker(edge.node)); 
+    markers = edges.map(edge => createPointMarker(edge.node)); 
   } else {
     console.log("Invalid input type.  Edges must be an array.");
   }
+  return markers;
 }
 
 function process(data) {
   if(data.allConnections && data.allConnections.edges.length > 0) {
-    const layer = createMarkers(data.allConnections.edges);
+    const markers = createMarkers(data.allConnections.edges);
+    const properties = {
+      imagePath: './images/m',
+      maxZoom: 15
+    };
+
+    new MarkerClusterer(myMap, markers, properties);
   }
 }
 
