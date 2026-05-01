15f38430bb7bc7606b14555bf9c4247c77a5c44d
Steve Beaty <drsjb80@gmail.com>
2026-05-01 07:54:54 -0600

    Added my coding preferences and fixes for TLS.

--- src/database.py
+++ src/database.py
@@ -9,15 +9,15 @@ from sqlalchemy_utils import database_exists, create_database
 from src.tables import Base
 from src.logger import logger
 
-db_thread_lock = threading.Lock()
 
 
 class Database:
     """Read from the config.yml file (if it exists) and set up the database connection."""
 
+
     def __init__(self, config):
         self.config = config
-        self.lock_needed = False
+        self.lock = threading.Lock()
         self.engine = None
 
     def _get_database_string(self):
@@ -44,12 +44,14 @@ class Database:
         return f'{database_type}://{database_user}{password_part}@{database_host}{port_part}{name_part}'
 
     def write(self, table):
-        """Write into the database, with locking if necessary."""
+        """Write into the database with a global lock."""
         with self.lock:
-            with Session(self.engine) as session:
+            session = Session(bind=self.engine)
+            try:
                 session.add(table)
                 session.commit()
-                 
+            finally:
+                session.close()
   
     def open(self):
         """Open the database connection and create database if it doesn't exist."""
